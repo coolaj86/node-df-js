e23ade403f41981323a8a038723d0a7f0aa39607
Fitnerd <sesshafter@yahoo.de>
2014-10-02 10:45:37 +0200

    added compatibility for Darwin
    Added additional regex and if for splitting linux and darwin.

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
   // map auto_home          0         0         0   100%    /home
   var exec = require('child_process').exec
     , os = require('os')
-    , reCaptureCells = /^([^\s]+\s?[^\s]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+[^/]*(.*?)\s+(.*?)\s*$/
+    , reCaptureCellsDarwin = /^([^\s]+\s?[^\s]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+[^/]*(.*?)\s*$/;
+    , reCaptureCellsLinux = /^([^\s]+\s?[^\s]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+[^/]*(.*?)\s+(.*?)\s*$/
     , cmdMap = {
           "Darwin": "df -b"
         , "Linux": "df -B 512 --output=source,size,used,avail,pcent,target,fstype"
@@ -39,15 +40,26 @@
         // FYI
         // It seems that RegExp Match isn't a true array
         // popping it deos weird things
-        infos.push({
-            filesystem: cells[1]
-          , blocks: parseInt(cells[2], 10)
-          , used: parseInt(cells[3], 10)
-          , available: parseInt(cells[4], 10)
-          , percent: parseInt(cells[5], 10)
-          , mountpoint: cells[6]
-          , type: cells[7]
-        });
+        if(os.type() == "Darwin") {
+          infos.push({
+              filesystem: cells[1]
+            , blocks: parseInt(cells[2], 10)
+            , used: parseInt(cells[3], 10)
+            , available: parseInt(cells[4], 10)
+            , percent: parseInt(cells[5], 10)
+            , mountpoint: cells[6]
+          });
+        } else {
+          infos.push({
+              filesystem: cells[1]
+            , blocks: parseInt(cells[2], 10)
+            , used: parseInt(cells[3], 10)
+            , available: parseInt(cells[4], 10)
+            , percent: parseInt(cells[5], 10)
+            , mountpoint: cells[6]
+            , type: cells[7]
+          });
+        }
       }
 
       if (err) {
